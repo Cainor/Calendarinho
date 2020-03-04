5922b6302e4397297eaa04b4cbb794bdda1bc518
Cainor <mmalmazroa@gmail.com>
2020-03-04 13:29:01 +0300

    service type as table

--- CalendarinhoApp/admin.py
+++ CalendarinhoApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Employee, Engagement, Leave, Client
+from .models import Employee, Engagement, Leave, Client, Services
 
 admin.site.register(Employee)
 admin.site.register(Client)
 admin.site.register(Engagement)
 admin.site.register(Leave)
+admin.site.register(Services)
--- CalendarinhoApp/models.py
+++ CalendarinhoApp/models.py
@@ -1,7 +1,119 @@
 from django.db import models
 import datetime
 from collections import namedtuple
-from users.models import CustomUser as Employee
+
+
+class Employee(models.Model):
+    EmpName = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.EmpName
+
+    def getAllLeaves(self):
+        event_arr = []
+        all_leaves = Leave.objects.filter(emp_id=self.id)
+        colors = ["#2C3E50", "#2980B9"]
+        for i in all_leaves:
+            event_sub_arr = {}
+            event_sub_arr['empName'] = self.EmpName
+            event_sub_arr['title'] = i.Note + " -- " + i.LeaveType
+            start_date = datetime.datetime.strptime(
+                str(i.StartDate), "%Y-%m-%d").strftime("%Y-%m-%d")
+            end_date = datetime.datetime.strptime(
+                str(i.EndDate), "%Y-%m-%d").strftime("%Y-%m-%d")
+            event_sub_arr['start'] = start_date
+            event_sub_arr['end'] = end_date
+            event_sub_arr['id'] = i.id
+            event_sub_arr['color'] = colors[0] if i.LeaveType == "Vacation" else colors[1]
+            event_arr.append(event_sub_arr)
+        return event_arr
+
+    def getAllEngagements(self):
+        event_arr = []
+        all_engagements = self.Engagements.all()
+        colors = ["#BD4932"]
+        for i in all_engagements:
+            event_sub_arr = {}
+            event_sub_arr['name'] = i.EngName
+            start_date = datetime.datetime.strptime(
+                str(i.StartDate), "%Y-%m-%d").strftime("%Y-%m-%d")
+            end_date = datetime.datetime.strptime(
+                str(i.EndDate), "%Y-%m-%d").strftime("%Y-%m-%d")
+            event_sub_arr['startDate'] = start_date
+            event_sub_arr['endDate'] = end_date
+            event_sub_arr['engID'] = i.id
+            event_sub_arr['color'] = colors[0]
+            event_sub_arr['serviceType'] = str(i.ServiceType)
+            event_sub_arr['clientName'] = i.CliName
+            event_arr.append(event_sub_arr)
+        return event_arr
+
+    # This method check if employee is availabile at a range of date
+    def overlapCheck(self, StartDate, EndDate):
+        engs = Engagement.objects.filter(Employees=self.id)
+        leaves = Leave.objects.filter(emp_id=self.id)
+        Range = namedtuple('Range', ['start', 'end'])
+        start_date1 = datetime.datetime.strptime(str(StartDate), "%Y-%m-%d")
+        end_date1 = datetime.datetime.strptime(str(EndDate), "%Y-%m-%d")
+        Range1 = Range(start=start_date1, end=end_date1)
+        event_dates = {"start_date": [], "end_date": []}
+        for eng in engs:
+            event_dates["start_date"].append(eng.StartDate)
+            event_dates["end_date"].append(eng.EndDate)
+
+        for lev in leaves:
+            event_dates["start_date"].append(lev.StartDate)
+            event_dates["end_date"].append(lev.EndDate)
+
+        for i in range(len(event_dates["start_date"])):
+            start_date2 = datetime.datetime.strptime(
+                str(event_dates["start_date"][i]), "%Y-%m-%d")
+            end_date2 = datetime.datetime.strptime(
+                str(event_dates["end_date"][i]), "%Y-%m-%d")
+            latestStart = max(start_date1, start_date2)
+            earliestEnd = min(end_date1, end_date2)
+            delta = (earliestEnd - latestStart).days + 1
+            overlapDays = max(0, delta)
+            if (overlapDays != 0):
+                return True
+
+    def dateInRange(self, date, fromDate, toDate):
+        if fromDate <= datetime.date(date.year, date.month, date.day) <= toDate:
+            return True
+        else:
+            return False
+
+    def currentStatus(self):
+        todayDate = datetime.datetime.now()
+        leaves = Leave.objects.filter(emp_id=self.id)
+        for lev in leaves:
+            if (self.dateInRange(todayDate, lev.StartDate, lev.EndDate)):
+                return [lev.LeaveType, lev.__str__(), lev.EndDate]
+
+        engs = Engagement.objects.filter(Employees=self.id)
+        for eng in engs:
+            if (self.dateInRange(todayDate, eng.StartDate, eng.EndDate)):
+                return ["Engaged", eng.__str__(), eng.EndDate]
+
+        return ["Available", "", "-"]
+
+    def nextEvent(self):
+        eng = Engagement.objects.filter(Employees=self.id).filter(
+            StartDate__gt=datetime.datetime.now().strftime("%Y-%m-%d")).order_by("StartDate").first()
+        lev = Leave.objects.filter(emp_id=self.id).filter(
+            StartDate__gt=datetime.datetime.now().strftime("%Y-%m-%d")).order_by("StartDate").first()
+        try:
+            if(eng.StartDate > lev.StartDate):
+                return [lev.__str__(), lev.StartDate]
+            else:
+                return [eng.__str__(), eng.StartDate]
+        except:
+            if(eng == None and lev == None):
+                return["None", "-"]
+            elif(eng == None):
+                return [lev.__str__(), lev.StartDate]
+            else:
+                return [eng.__str__(), eng.StartDate]
 
 
 class Client(models.Model):
@@ -12,15 +124,23 @@ class Client(models.Model):
         return str(self.CliName)
 
 
+    
+
+class Services(models.Model):
+    serviceName = models.CharField(max_length=200)
+    serviceShort = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.serviceName)
+
+
+
 class Engagement(models.Model):
     EngName = models.CharField(max_length=200)
     CliName = models.ForeignKey(Client, on_delete=models.PROTECT)
     Employees = models.ManyToManyField(
-        Employee, blank=True, related_name="Engagements")
-    services = (("Non", "--"), ("WVA", "Web Vulnerability Asseessment"), ("WPT", "Web Penetration Testing"), ("SCR",
-                                                                                                              "Source Code Review"), ("NVA", "Network Vulnerability Asseessment"), ("NPT", "Network Penetration Testing"))
-    ServiceType = models.CharField(
-        max_length=3, choices=services, default="Non")
+        "Employee", blank=True, related_name="Engagements")
+    ServiceType = models.ForeignKey(Services, on_delete=models.PROTECT)
     StartDate = models.DateField('Start Date')
     EndDate = models.DateField('End Date')
 
@@ -31,7 +151,7 @@ class Engagement(models.Model):
                   "#9E0034", "#B7410E", "#446600"]
         for i in all_events:
             event_sub_arr = {}
-            event_sub_arr['title'] = i.EngName + " -- " + i.ServiceType
+            event_sub_arr['title'] = i.EngName + " -- " + str(i.ServiceType)
             start_date = datetime.datetime.strptime(
                 str(i.StartDate), "%Y-%m-%d").strftime("%Y-%m-%d")
             end_date = datetime.datetime.strptime(
@@ -44,7 +164,7 @@ class Engagement(models.Model):
         return event_arr
 
     def __str__(self):
-        return str(self.EngName + " - " + self.ServiceType)
+        return str(self.EngName) + " - " + str(self.ServiceType)
 
     def daysLeftPrecentage(self):
         todayDatetiem = datetime.date.today()
@@ -72,6 +192,7 @@ class Engagement(models.Model):
             # The -1 is to get positive numbers.
             return daysLeft * -1
         return"Nope"
+
 
 
 class Leave(models.Model):
