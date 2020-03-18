7ea5cc6bebb2bb916b7b1d7d2cb624afe7aeda53
Cainor <mmalmazroa@gmail.com>
2020-03-18 21:29:21 +0300

    admin.py CustomClasses

--- CalendarinhoApp/admin.py
+++ CalendarinhoApp/admin.py
@@ -3,10 +3,42 @@ from django.contrib import admin
 from .models import Employee, Engagement, Leave, Client, Service, Comment
 
 admin.site.register(Employee)
-admin.site.register(Client)
-admin.site.register(Engagement)
-admin.site.register(Leave)
-admin.site.register(Service)
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('CliName', 'CliShort', 'CliCode')
+    search_fields = ('CliName', 'CliShort')
+
+
+admin.site.register(Client, ClientAdmin)
+
+
+class EngagementAdmin(admin.ModelAdmin):
+    list_display = ('EngName', 'CliName', 'ServiceType',
+                    'StartDate', 'EndDate')
+    list_filter = ('ServiceType', 'StartDate')
+    search_fields = ('EngName', 'CliName__CliName')
+
+
+admin.site.register(Engagement, EngagementAdmin)
+
+
+class LeaveAdmin(admin.ModelAdmin):
+    list_display = ('Note', 'emp', 'LeaveType', 'StartDate', 'EndDate')
+    list_filter = ('LeaveType', 'StartDate', 'EndDate')
+    search_fields = ('Note', 'emp__first_name', 'emp__last_name')
+
+
+admin.site.register(Leave, LeaveAdmin)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('serviceName', 'serviceShort')
+    search_fields = ('serviceName', 'serviceShort')
+
+
+# if i added more serch fiels will generate error
+admin.site.register(Service, ServiceAdmin)
 
 
 class CommentAdmin(admin.ModelAdmin):
--- users/admin.py
+++ users/admin.py
@@ -7,15 +7,17 @@ from django.utils.translation import gettext, gettext_lazy as _
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = ['username', 'first_name', 'last_name', 'user_type']
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'user_type')}),
+        (_('Personal info'), {
+         'fields': ('first_name', 'last_name', 'email', 'user_type')}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
@@ -24,9 +26,11 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2','first_name', 'last_name', 'email'),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email'),
         }),
     )
+    search_fields = ('username', 'first_name', 'last_name', 'user_type')
+
 
 admin.site.unregister(CustomUser)
 admin.site.register(CustomUser, CustomUserAdmin)
