1b8d555bfeb0bb69fecf628f48cc472aeaf0f111
Cainor <mmalmazroa@gmail.com>
2025-01-12 14:59:18 +0300

    fix missing variables

--- generate-cert.sh
+++ generate-cert.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker run --rm -v $(pwd)/ssl:/certs alpine/openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /certs/private.key -out /certs/certificate.crt \
-    -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
+docker run --rm -v $(pwd)/ssl:/certs alpine/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /certs/private.key -out /certs/certificate.crt -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
